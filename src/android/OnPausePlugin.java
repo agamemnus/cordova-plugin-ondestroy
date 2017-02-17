6982ddbbdbc38fce2a611ea70b4c082c732215e1
agamemnus <agamemnus@users.noreply.github.com>
2017-02-17 18:49:28 -0500

    Create OnPausePlugin.java

---
+++ src/android/OnPausePlugin.java
@@ -0,0 +1,32 @@
+package com.flyingsoftgames.ondestroyplugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+
+public class OnDestroyPlugin extends CordovaPlugin {
+ 
+ private CallbackContext onDestroyCallback = null;
+ private final String LOG_TAG = "onPausePlugin";
+ 
+ public boolean execute (String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+  onPauseCallback = callbackContext;
+  return true;
+ }
+ 
+ @Override public void onPause () {
+  if (onPauseCallback != null) onPauseCallback.sendPluginResult (pluginResultKeep());
+  super.onPause ();
+ };
+ 
+ private PluginResult pluginResultKeep () {
+  PluginResult pluginResult = new PluginResult (PluginResult.Status.OK);
+  pluginResult.setKeepCallback (true);
+  return pluginResult;
+ }
+}
