d640d8b1e00b4bc329d126f9731279f3bb615e52
agamemnus <agamemnus@users.noreply.github.com>
2017-02-17 19:46:19 -0500

    Update ondestroyplugin.js

--- www/ondestroyplugin.js
+++ www/ondestroyplugin.js
@@ -2,8 +2,8 @@ module.exports = function () {
  exports.setEventListener = exports.setDestroyEventListener = function (callback) {
   cordova.exec (callback, undefined, "OnDestroyPlugin", "setEventListener", [])
  }
- exports.setOnPauseEventListener = function (callback) {
-  cordova.exec (callback, undefined, "OnPausePlugin", "setEventListener", [])
+ exports.addOnPauseEventListener = function (callback) {
+  document.addEventListener("pause", callback, false)
  }
  return exports
 } ()
